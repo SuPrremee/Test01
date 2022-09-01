f4cf82d6c837e1d9ce4bd62b1bcd2897672ccfcf
SuPrremee <1071809541@qq.com>
2022-09-01 19:35:36 +0800

    second commit

--- src/com/qiao/ooptest/Test/gitTest.java
+++ src/com/qiao/ooptest/Test/gitTest.java
@@ -10,5 +10,6 @@ package com.qiao.ooptest.Test;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
